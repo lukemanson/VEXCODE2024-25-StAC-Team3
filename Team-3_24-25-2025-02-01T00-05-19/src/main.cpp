87e15e98bbd96d9e73d6cd17cb2331d216fb41a5
lukemanson <158853393+lukemanson@users.noreply.github.com>
2025-03-08 11:01:52 +1300

    e
    e

--- Team-3_24-25-2025-02-01T00-05-19/src/main.cpp
+++ Team-3_24-25-2025-02-01T00-05-19/src/main.cpp
@@ -106,9 +106,14 @@ void auton_blue_negative(void) {
   driveForwardOrBack(0);
   wait(0.5, sec);
   intake_spinner.setVelocity(100, percent);
-  wait(4, sec);
+  wait(1.5, sec);
   intake.setVelocity(0, percent);
   intake_spinner.setVelocity(0, percent);
+  driveLeftOrRight(-50);
+  wait(0.6, sec);
+  driveForwardOrBack(-75);
+  wait(0.35, sec);
+  driveForwardOrBack(0);
   // driveLeftOrRight(20);
   // wait(1.1, sec);
   // driveLeftOrRight(0);
@@ -147,9 +152,14 @@ void auton_red_negative(void) {
   driveForwardOrBack(0);
   wait(0.5, sec);
   intake_spinner.setVelocity(100, percent);
-  wait(4, sec);
+  wait(1.5, sec);
   intake.setVelocity(0, percent);
   intake_spinner.setVelocity(0, percent);
+  driveLeftOrRight(50);
+  wait(0.6, sec);
+  driveForwardOrBack(-75);
+  wait(0.35, sec);
+  driveForwardOrBack(0);
   // intake.setVelocity(-100, percent);
   // intake_spinner.setVelocity(-100, percent);
   // wait(1.1, sec);
@@ -247,6 +257,115 @@ void auton_red_negative_awp(void) {
   driveForwardOrBack(0);
 }
 
+void skills(void) {
+  intake.setVelocity(-100, percent);
+  intake_spinner.setVelocity(-100, percent);
+  driveForwardOrBack(-40);
+  wait(1, sec);
+  intake.setVelocity(100, percent);
+  driveForwardOrBack(40);
+  wait(0.7, sec);
+  intake.setVelocity(-100, percent);
+  driveLeftOrRight(50);
+  wait(0.5, sec);
+  driveForwardOrBack(-80);
+  wait(0.4, sec);
+  driveForwardOrBack(0);
+  wait(0.5, sec);
+  goal_grabbed = goal_grab(goal_grabbed);
+  wait(0.5, sec);
+  driveLeftOrRight(40);
+  wait(1.3, sec);
+  driveForwardOrBack(50);
+  wait(1, sec);
+  driveForwardOrBack(30);
+  wait(1.5, sec);
+  driveForwardOrBack(0);
+  wait(1, sec);
+  driveForwardOrBack(-50);
+  wait(1.2, sec);
+  driveForwardOrBack(0);
+  wait(0.5, sec);
+  driveLeftOrRight(-20);
+  wait(0.5, sec);
+  driveLeftOrRight(0);
+  driveForwardOrBack(40);
+  wait(1, sec);
+  driveForwardOrBack(0);
+  wait(1.5, sec);
+  driveLeftOrRight(-50);
+  wait(1, sec);
+  driveForwardOrBack(-50);
+  wait(1, sec);
+  driveForwardOrBack(0);
+  goal_grabbed = goal_grab(goal_grabbed);
+  wait(0.5, sec);
+  driveForwardOrBack(50);
+  wait(1, sec);
+  driveForwardOrBack(0);
+}
+
+
+void auton_blue_positive(void) {
+  // Autonomous Program for positioning on the Red Alliance Negative Corner area
+  driveForwardOrBack(-50);
+  wait(1.8, sec);
+  driveForwardOrBack(0);
+  goal_grabbed = goal_grab(goal_grabbed);
+  driveForwardOrBack(50);
+  wait(0.2, sec);
+  driveForwardOrBack(0);
+  intake.setVelocity(-75, percent);
+  intake_spinner.setVelocity(-100, percent);
+  wait(1, sec);
+  driveLeftOrRight(-20);
+  intake.setVelocity(75, percent);
+  wait(0.8, sec);
+  intake.setVelocity(-75, percent);
+  wait(0.35, sec);
+  driveLeftOrRight(0);
+  driveForwardOrBack(50);
+  intake.setVelocity(-75, percent);
+  intake_spinner.setVelocity(-100, percent);
+  wait(1.2, sec);
+  driveForwardOrBack(0);
+  wait(0.5, sec);
+  intake_spinner.setVelocity(100, percent);
+  wait(4, sec);
+  intake.setVelocity(0, percent);
+  intake_spinner.setVelocity(0, percent);
+}
+
+void auton_red_positive(void) {
+  // Autonomous Program for positioning on the Blue Alliance Negative Corner area
+  driveForwardOrBack(-50);
+  wait(1.8, sec);
+  driveForwardOrBack(0);
+  goal_grabbed = goal_grab(goal_grabbed);
+  driveForwardOrBack(50);
+  wait(0.2, sec);
+  driveForwardOrBack(0);
+  intake.setVelocity(-75, percent);
+  intake_spinner.setVelocity(-100, percent);
+  wait(1, sec);
+  driveLeftOrRight(20);
+  intake.setVelocity(75, percent);
+  wait(0.8, sec);
+  intake.setVelocity(-75, percent);
+  wait(0.35, sec);
+  driveLeftOrRight(0);
+  driveForwardOrBack(50);
+  intake.setVelocity(-75, percent);
+  intake_spinner.setVelocity(-100, percent);
+  wait(1.2, sec);
+  driveForwardOrBack(0);
+  wait(0.5, sec);
+  intake_spinner.setVelocity(100, percent);
+  wait(4, sec);
+  intake.setVelocity(0, percent);
+  intake_spinner.setVelocity(0, percent);
+}
+
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
 /*                                                                           */
@@ -304,25 +423,25 @@ void autonomous(void) {
     auton_blue_negative_awp();
   }
   if (isRed == true && isPos == true && isAWP == false) {
-    auton_blue_negative();
+    auton_red_positive();
   }
   if (isRed == true && isPos == false && isAWP == true) {
     auton_red_negative_awp();
   }
   if (isRed == true && isPos == false && isAWP == false) {
-    auton_red_negative();
+    auton_blue_positive();
   }
   if (isRed == false && isPos == true && isAWP == true) {
     auton_red_negative_awp();
   }
   if (isRed == false && isPos == true && isAWP == false) {
-    auton_red_negative();
+    auton_blue_positive();
   }
   if (isRed == false && isPos == false && isAWP == true) {
     auton_blue_negative_awp();
   }
   if (isRed == false && isPos == false && isAWP == false) {
-    auton_blue_negative();
+    auton_red_positive();
   }
 }
 
